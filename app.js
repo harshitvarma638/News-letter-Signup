bbf7fe11da020e6a5b171158a8aace4b868d855b
Harshith Varma <harshitvarma638@gmail.com>
2024-02-01 21:39:02 +0530

    updated with new api key

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const request = require("request");
 const https = require("https");
-const app = express()
-require('dotenv').config()
-const port = 3000
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,7 +12,7 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", (req,res)=>{
+app.post("/", (req, res) => {
   const firstName = req.body.fname;
   const lastName = req.body.lname;
   const email = req.body.email;
@@ -36,38 +35,41 @@ app.post("/", (req,res)=>{
   const url = "https://us21.api.mailchimp.com/3.0/lists/c11ffe20a7";
 
   const options = {
-     method: "POST",
-     auth: `harshith:${process.env.API_KEY}`
-  }
+    method: "POST",
+    auth: `harshith:${process.env.API_KEY}`
+  };
+  // console.log(process.env.API_KEY);
+  const request = https.request(url, options, function (response) {
+    let resultData = '';
 
-  const request = https.request(url,options,function(response){
+    response.on('data', function (chunk) {
+      resultData += chunk;
+    });
 
-    if(response.statusCode === 200){
-      res.sendFile(__dirname + "/success.html");
-    }
-    else{
-      res.sendFile(__dirname + "/failure.html");
-    }
-    response.on("data", function(data){
-      console.log(JSON.parse(data));
-    })
-  })
+    response.on('end', function () {
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+        console.error(`Mailchimp API Error: ${response.statusCode}`);
+        console.log(JSON.parse(resultData));
+      }
+    });
+  });
+
+  request.on('error', function (error) {
+    console.error('Request Error:', error);
+    res.sendFile(__dirname + "/failure.html");
+  });
 
   request.write(jsonData);
   request.end();
 });
 
-app.post("/failure", function(req,res){
+app.post("/failure", function (req, res) {
   res.redirect("/");
-})
+});
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-// mailchimp API key
-// 82315bfe3de2b9ed09e501e03bc2ab49-us21
-
-//audience id 
-// c11ffe20a7
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
